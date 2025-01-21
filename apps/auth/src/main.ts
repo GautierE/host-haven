250eee2829260b4e64be0db3d11928dbaa9398d9
Gautier <gautier.edel.info@gmail.com>
2025-01-21 22:59:00 +0100

    feat(auth): add JWT auth strategy & corresponding Guard

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,16 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
-import { Logger } from 'nestjs-pino';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { NestFactory } from "@nestjs/core";
+import { AuthModule } from "./auth.module";
+import { Logger } from "nestjs-pino";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
 
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
 
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT'));
+  await app.listen(configService.get("PORT"));
 }
 bootstrap();
