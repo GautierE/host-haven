63f28508c60e46f8eb8a1b3d092a51bca6bc8fd9
Gautier <gautier.edel.info@gmail.com>
2025-01-21 23:13:33 +0100

    feat(create user): ensure user with same email doesnt already exists

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,18 +1,34 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from "@nestjs/common";
 import * as bcrypt from "bcryptjs";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { UsersRepository } from "./users.repository";
 import { GetUserDto } from "./dto/get-user.dto";
+import { UsersRepository } from "./users.repository";
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   async create(createUserDto: CreateUserDto) {
+    await this.validateCreateUserDto(createUserDto);
+
     return this.usersRepository.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 12),
     });
+  }
+
+  private async validateCreateUserDto(createUserDto: CreateUserDto) {
+    const user = await this.usersRepository.findOne({
+      email: createUserDto.email,
+    });
+
+    if (user) {
+      throw new UnprocessableEntityException("Email already exists");
+    }
   }
 
   async validateUser(email: string, password: string) {
