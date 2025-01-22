27af6051620077c823e6bd7ed2dd124d5ed21ff1
Gautier EDEL <gautier@tool4staffing.com>
2025-01-22 21:44:02 +0100

    [WIP] feat(auth): add auth guard to reservations service routes

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -4,18 +4,24 @@ import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { CurrentUser } from "./current-user.decorator";
 import { UserDocument } from "./users/models/user.schema";
 import { Response } from "express";
+import { MessagePattern } from "@nestjs/microservices";
+import { JwtAuthGuard } from "@app/common";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) { }
 
-  @UseGuards(LocalAuthGuard)
-  @Post("login")
-  async login(
-    @CurrentUser() user: UserDocument,
-    @Res({ passthrough: true }) response: Response,
-  ) {
-    await this.authService.login(user, response);
-    response.send(user);
-  }
+    @UseGuards(LocalAuthGuard)
+    @Post("login")
+    async login(
+        @CurrentUser() user: UserDocument,
+        @Res({ passthrough: true }) response: Response,
+    ) {
+        await this.authService.login(user, response);
+        response.send(user);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @MessagePattern('authenticate')
+    async authenticate() { }
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,28 +4,33 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationsRepository } from './reservations.repository';
 import {
-  ReservationDocument,
-  ReservationSchema,
+    ReservationDocument,
+    ReservationSchema,
 } from './models/reservation.schema';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from 'joi';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AUTH_SERVICE } from '@app/common';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        MONGODB_URI: Joi.string().required(),
-        PORT: Joi.number().required(),
-      }),
-    }),
-    DatabaseModule,
-    DatabaseModule.forFeature([
-      { name: ReservationDocument.name, schema: ReservationSchema },
-    ]),
-    LoggerModule,
-  ],
-  controllers: [ReservationsController],
-  providers: [ReservationsService, ReservationsRepository],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required(),
+                PORT: Joi.number().required(),
+            }),
+        }),
+        DatabaseModule,
+        DatabaseModule.forFeature([
+            { name: ReservationDocument.name, schema: ReservationSchema },
+        ]),
+        LoggerModule,
+        ClientsModule.register([
+            { name: AUTH_SERVICE, transport: Transport.TCP }
+        ])
+    ],
+    controllers: [ReservationsController],
+    providers: [ReservationsService, ReservationsRepository],
 })
-export class ReservationsModule {}
+export class ReservationsModule { }
