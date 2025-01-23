1a62b43e38ee186ecae914a27a0866fbdd1824f6
Gautier <gautier.edel.info@gmail.com>
2025-01-23 22:49:09 +0100

    feat(payments): add stripe support

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import Stripe from "stripe";
 
 @Injectable()
 export class PaymentsService {
+  private readonly stripe = new Stripe(
+    this.configService.get("STRIPE_SECRET_KEY"),
+    {
+      apiVersion: "2024-12-18.acacia",
+    },
+  );
+
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
   }
 }
