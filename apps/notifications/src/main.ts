ec551d49083c5e6b4b19cea86dee4207020dfedb
Gautier <gautier.edel.info@gmail.com>
2025-01-26 15:15:11 +0100

    feat(notifications): send email on payment charge

--- apps/notifications/src/main.ts
+++ apps/notifications/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { NotificationsModule } from './notifications.module';
+import { NestFactory } from "@nestjs/core";
+import { ConfigService } from "@nestjs/config";
+import { Transport } from "@nestjs/microservices";
+import { Logger } from "nestjs-pino";
+import { NotificationsModule } from "./notifications.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(NotificationsModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: "0.0.0.0",
+      port: configService.get("TCP_PORT"),
+    },
+  });
+
+  app.useLogger(app.get(Logger));
+  await app.startAllMicroservices();
 }
 bootstrap();
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,7 +1,9 @@
-import { CreateChargeDto } from "@app/common";
-import { Injectable, UsePipes, ValidationPipe } from "@nestjs/common";
+import { CreateChargeDto, NOTIFICATIONS_SERVICE } from "@app/common";
+import { Inject, Injectable, UsePipes, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { ClientProxy } from "@nestjs/microservices";
 import Stripe from "stripe";
+import { PaymentsCreateChargeDto } from "./dto/payments-create-charge.dto";
 
 @Injectable()
 export class PaymentsService {
@@ -12,16 +14,27 @@ export class PaymentsService {
     },
   );
 
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
 
   @UsePipes(new ValidationPipe())
-  async createCharge({ card, amount }: CreateChargeDto) {
-    return await this.stripe.paymentIntents.create({
+  async createCharge({ card, amount, email }: PaymentsCreateChargeDto) {
+    const paymentIntent = await this.stripe.paymentIntents.create({
       payment_method: "pm_card_visa",
       amount: amount * 100,
       confirm: true,
       payment_method_types: ["card"],
       currency: "eur",
     });
+
+    this.notificationsService.emit("notify_email", {
+      email,
+      text: `Your payment of ${amount}$  was successful`,
+    });
+
+    return paymentIntent;
   }
 }
