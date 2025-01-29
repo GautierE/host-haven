212f5e1be82b6993b4101abc818f83caf9d14a47
Gautier <gautier.edel.info@gmail.com>
2025-01-29 18:17:37 +0100

    refactor: move user.schema to common directory

--- apps/auth/src/users/users.repository.ts
+++ apps/auth/src/users/users.repository.ts
@@ -1,8 +1,7 @@
-import { AbstractRepository } from '@app/common';
-import { Injectable, Logger } from '@nestjs/common';
-import { UserDocument } from './models/user.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { AbstractRepository, UserDocument } from "@app/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 @Injectable()
 export class UsersRepository extends AbstractRepository<UserDocument> {
