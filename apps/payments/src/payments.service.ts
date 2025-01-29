6faf92cd61945752060b80bb103ed11ff5b5ca4f
Gautier <gautier.edel.info@gmail.com>
2025-01-29 19:01:42 +0100

    chore: remove payments dependencies from global package.json

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -10,7 +10,7 @@ export class PaymentsService {
   private readonly stripe = new Stripe(
     this.configService.get("STRIPE_SECRET_KEY"),
     {
-      apiVersion: "2024-12-18.acacia",
+      apiVersion: "2025-01-27.acacia",
     },
   );
 
